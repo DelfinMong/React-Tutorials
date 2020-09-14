dd6886945621fefb3b91ae990ba1541a71079d45
delfin mongendu <delfdevs@gmail.com>
2020-09-14 01:18:25 -0400

    game-component

--- Pokedex/pokedex/src/Pokedex.js
+++ Pokedex/pokedex/src/Pokedex.js
@@ -20,11 +20,21 @@ class Pokedex extends Component {
     };
     
     render() {
+
+        let title;
+        if ( this.props.isWinning){
+            title = <h1 className='Pokedex-winner'>Winning Hand</h1>;
+        }else{
+            title = <h1 className='Pokedex-loser'>Losing Hand</h1>;
+        }
+        
         return (
             <div className="Pokedex">
                 <h1>Pokedex!</h1>
+                <p>Total Experience: {this.props.exp} </p>
+                { title }
                 <div className="Pokedex-cards">
-                    { this.props.pokemon.map((p) => (
+                    { this.props.pokeman.map((p) => (
                         <Pokecard id = {p.id} name = {p.name} type = {p.type} exp = {p.base_experience} />
                     ))}
                 </div>       
