0e8b43ee084d22e923aa18a9ef509a7409efcd2e
delfin mongendu <delfdevs@gmail.com>
2020-09-14 23:59:21 -0400

    player

--- scoreboard/src/Header.js
+++ scoreboard/src/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Stats from './Stats';
 
-const Header = ( {title, totalPlayers} ) => {
+const Header = (props) => {
     return (
         <header>
-            <h1>{ title }</h1>
-            <span className='stats'> Players: { totalPlayers }</span>
+            <Stats />
+            <h1>{ props.title }</h1>
         </header>
     )
 }
