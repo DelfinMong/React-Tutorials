7ced8ffa39546a93e2876b911a8d0ade98972818
delfin mongendu <delfdevs@gmail.com>
2020-09-19 21:42:01 -0400

    stopwatch2

--- scoreboard/src/Stopwatch.js
+++ scoreboard/src/Stopwatch.js
@@ -2,12 +2,25 @@ import React, { Component } from 'react';
 
 
 class Stopwatch extends Component {
+    
+    state = {
+        isRunning: false
+    };
+
+    handleStopwatch = () => {
+        this.setState({
+            isRunning: !this.state.isRunning
+        })
+    }
+
     render() {
         return (
             <div className="stopwatch">
                 <h2>Stopwatch</h2>
                 <span className="stopwatch-time">0</span>
-                <button>Start</button> 
+                <button onClick={ this.handleStopwatch }>
+                    { this.state.isRunning ? 'Stop' : 'Start'}
+                </button> 
                 <button>Reset</button>
             </div>
         )
@@ -15,3 +28,23 @@ class Stopwatch extends Component {
 }
 
 export default Stopwatch;
+
+
+
+// render() {
+//     let button;
+//     if (this.state.isRunning) {
+//       button = <button>Stop</button>;
+//     } else {
+//       button = <button>Start</button>; 
+//     }
+  
+//     return (
+//       <div className="stopwatch">
+//         <h2>Stopwatch</h2>
+//         <span className="stopwatch-time">...</span>
+//         { button }
+//         <button onClick={this.handleReset}>Reset</button>
+//       </div>
+//     );
+//   }
