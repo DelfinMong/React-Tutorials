b01284f7d910e258d6a66825c0f5c255d69e0b34
delfin mongendu <delfdevs@gmail.com>
2020-09-27 23:56:18 -0400

    roll_dice

--- Dice/dice/src/App.js
+++ Dice/dice/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Die from './Die'
+import RollDice from './RollDice'
 import './App.css';
 // import RollDice from './RollDice';
 
@@ -8,10 +8,7 @@ class App extends Component {
     
   return (
     <div className="App">
-       <Die face="five"/>
-       <Die face="six"/>
-       <Die face="three"/>
-       <Die face="two"/>
+       <RollDice />
     </div>
    );
   }
