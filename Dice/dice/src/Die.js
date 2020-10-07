19cce2ff6fa8ef3617a6283e7d2efdcc8b991e98
delfin mongendu <delfdevs@gmail.com>
2020-10-07 04:56:51 -0400

    animation_correction

--- Dice/dice/src/Die.js
+++ Dice/dice/src/Die.js
@@ -4,8 +4,12 @@ import './Die.css'
 class Die extends Component {
     render() {
         // let cls = `fas fa-dice-${this.props.face}`
-        return <i className={`Die fas fa-dice-${this.props.face}`} />
-           
+        return (
+            <i 
+            className={`Die fas fa-dice-${this.props.face} ${this.props.rolling && 
+                "shaking"}`}
+            />
+        )
     }
 }
 
--- Dice/dice/src/RollDice.js
+++ Dice/dice/src/RollDice.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
-import Die from './Die'
-import './RollDice.css'
+import Die from './Die';
+import './RollDice.css';
+import './Die.css'
 
 
 class RollDice extends Component {
@@ -10,44 +11,48 @@ class RollDice extends Component {
 
     constructor(props){
         super(props);
-        this.state = {die1:'one', die2:'one', rolling: false};
+        this.state = { 
+            die1:'one', 
+            die2:'one', 
+            rolling: false
+        };
         this.roll = this.roll.bind(this);
     }
 
     roll(){
         // pick 2 new rolls
         const newDie1 = this.props.sides [
-                Math.floor(Math.random() * this.props.sides.length)
+            Math.floor(Math.random() * this.props.sides.length)
         ];
 
         const newDie2 = this.props.sides [
             Math.floor(Math.random() * this.props.sides.length)
         ];
+        
         // set state with new rolls
-
         this.setState({ 
-               die1: newDie1, 
-               die2: newDie2,
-               rolling: true
-        })
+            die1: newDie1, 
+            die2: newDie2,
+            rolling: true
+        });
 
-        setTimeout( () => {
-            this.setState({ rolling: false})
+        // wait one second, then set rolling to false
+        setTimeout(() => {
+            this.setState({ rolling: false});
         }, 1000);
     }
-
     render () {
         return(
             <div className='RollDice'>
                 <div className='RollDice-container'>
-                    <Die face={this.state.die1}/>
-                    <Die face={this.state.die2}/>
+                    <Die face={this.state.die1} rolling={this.state.rolling}/>
+                    <Die face={this.state.die2} rolling={this.state.rolling}/>
                 </div>
-                <button onClick={ this.roll } disabled={this.state.rolling}>
-                  {this.state.rolling? "Rolling..." : "Roll Dice!"}
+                <button onClick={this.roll} disabled={this.state.rolling}>
+                  {this.state.rolling ? "Rolling..." : "Roll Dice!"}
                 </button>
             </div>
-        )
+        );
     }
 }
 
