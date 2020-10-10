f2bb770a6789eee043c00e40d268780b0d6f304d
delfin mongendu <delfdevs@gmail.com>
2020-10-09 21:25:26 -0400

    react_router_starter

---
+++ SPA/course-dir/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom'
+import Home from './Home'
+import About from './About'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="container">
+        <Route exact path="/" component={Home}/>
+        <Route path="/about" component={About}/>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
