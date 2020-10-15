f7877334f55c2a6015fa7a416b9c276853a08d04
delfin mongendu <delfdevs@gmail.com>
2020-10-14 23:59:21 -0400

    error

--- Coin-Flipper/src/App.js
+++ Coin-Flipper/src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class CoinContainer extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      currCoin: null,
+      nFlips: 0,
+      nHeads: 0,
+      
+    }
   return (
     <div className="App">
-     
+      
     </div>
   );
+}
 }
 
 export default App;
---
+++ Coin-Flipper/src/CoinContainer.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+
+class CoinContainer extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      currCoin: null,
+      nFlips: 0,
+      nHeads: 0,
+      nTails: 0
+    }
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+}
+
+export default CoinContainer;
