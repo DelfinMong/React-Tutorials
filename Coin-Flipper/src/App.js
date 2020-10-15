9743dc7c826f9ebea53e7176e39e3fc296c263d0
delfin mongendu <delfdevs@gmail.com>
2020-10-15 00:46:25 -0400

    container_render

--- Coin-Flipper/src/App.js
+++ Coin-Flipper/src/App.js
@@ -1,22 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import CoinContainer from './CoinContainer'
+
 import './App.css';
 
-class CoinContainer extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      currCoin: null,
-      nFlips: 0,
-      nHeads: 0,
-      
-    }
+class App extends Component {
+  render() {
+    
   return (
     <div className="App">
-      
+      <CoinContainer />
     </div>
-  );
-}
+   );
+  }
 }
 
 export default App;
--- Coin-Flipper/src/CoinContainer.js
+++ Coin-Flipper/src/CoinContainer.js
@@ -1,7 +1,14 @@
+import { render } from '@testing-library/react';
 import React, { Component } from 'react';
 
 
 class CoinContainer extends Component {
+ static defaultProps = {
+     coins: [
+        { side: "heads", imgSrc: "https://tinyurl.com/react-coin-heads-jpg" },
+        { side: "tails", imgSrc: "https://tinyurl.com/react-coin-tails-jpg" }
+     ]
+ }
   constructor(props){
     super(props);
     this.state = {
@@ -10,9 +17,14 @@ class CoinContainer extends Component {
       nHeads: 0,
       nTails: 0
     }
+    
+}
+  render() {
   return (
-    <div className="App">
-      
+    <div className="CoinContainer">
+    <h2>Let's Flip A Coin!</h2>
+    <p>Out of {this.state.nFlips} flips, there have been {this.state.nHeads} heads 
+       and {this.state.nTails} tails.</p>
     </div>
   );
 }
