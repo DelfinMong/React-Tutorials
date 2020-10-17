1837189218613750b31cb31691aa89045216020d
delfin mongendu <delfdevs@gmail.com>
2020-10-16 22:20:07 -0400

    helper

--- Coin-Flipper/src/CoinContainer.js
+++ Coin-Flipper/src/CoinContainer.js
@@ -1,5 +1,6 @@
 import { render } from '@testing-library/react';
 import React, { Component } from 'react';
+import { choice } from './helper'
 
 
 class CoinContainer extends Component {
@@ -16,15 +17,22 @@ class CoinContainer extends Component {
       nFlips: 0,
       nHeads: 0,
       nTails: 0
-    }
-    
-}
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  flipCoin(){
+    const newCoin = choice(this.props.coins);
+  }
+  handleClick(e){
+    this.flipCoin()
+  }
   render() {
   return (
     <div className="CoinContainer">
-    <h2>Let's Flip A Coin!</h2>
-    <p>Out of {this.state.nFlips} flips, there have been {this.state.nHeads} heads 
-       and {this.state.nTails} tails.</p>
+      <h2>Let's Flip A Coin!</h2>
+      <button onClick={this.handleClick}>Flip Me!</button>
+      <p>Out of {this.state.nFlips} flips, there have been {this.state.nHeads} heads 
+        and {this.state.nTails} tails.</p>
     </div>
   );
 }
